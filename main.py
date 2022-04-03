1e2fe606e489fa6df3d59ccf40f5dfa171946b21
Abdul Hannan Ghafoor <ahannan.ghafoor@gmail.com>
2022-04-03 20:42:06 +0500

    cleaning up the main file. including the arguments in the main file

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ import util
 import os
 from mpi4py import MPI
 
+
+parser = argparse.ArgumentParser(description='Count the number of languages in a twitter dataset')
+parser.add_argument('--twitter_data_file_path', type=str, default='./bigTwitter.json', help='path to the twitter dataset file in the json format')
+parser.add_argument('--grid_file_path', type=str, default='./data/sydGrid.json', help='path to the grid file which contains the grid information')
+parser.add_argument('--language_file', type=str, default='./data/language-codes_csv.csv', help='path to the language codes file which contains the information about different language codes')
+parser.add_argument('--batch_size', type=int, default=1000, help='Number of tweets that a subprocesses handles at a time')
+parser.add_argument('--out_file', type=str, default='./results/output.csv', help='the file name for the results along with the path')
+args = parser.parse_args()
+
+
+
 COMM = MPI.COMM_WORLD
 process_size = COMM.Get_size()
 process_rank = COMM.Get_rank()
@@ -16,25 +27,12 @@ if process_rank == 0:
 
 twitter_rows = 0
 ##### Need to use the arguments to get file path and file name. This feels hardcoded for now.
-twitter_data_file= open('./data/smallTwitter.json', 'r', encoding='utf-8')
+twitter_data_file= open(args.twitter_data_file_path, 'r', encoding='utf-8')
 chunk_sizes = []
 if process_rank == 0:
-    file_size= os.path.getsize('./data/smallTwitter.json')
+    file_size= os.path.getsize(args.twitter_data_file_path)
     per_process = file_size/process_size
     twitter_data_rows=twitter_data_file.readline()
-
-    # This is the code of calculating the number of tweets. Since we are doing things using the byte marker, calculating
-    # the nnumber of rows feel redundant
-
-    #####
-    #if ',"rows":[\n' in twitter_data_rows:
-    #    twitter_data_rows = twitter_data_rows.rstrip(',"rows":[\n')
-    #    twitter_data_rows = twitter_data_rows+'}'
-    #    twitter_data_rows = json.loads(twitter_data_rows)
-    #    twitter_rows = int(twitter_data_rows['total_rows'])
-    #    if 'offset' in twitter_data_rows:
-    #        twitter_rows = twitter_rows + int(twitter_data_rows['offset'])
-    #####
 
     # Below code could be put in the function.
     for index in range(process_size):
@@ -48,18 +46,11 @@ if process_rank == 0:
         twitter_data_file.readline()
 
 
-# loading all the files here.
-#twitter_data_file = open('./smallTwitter.json', 'r', encoding='utf-8')
-#twitter_data_file_json = ijson.items(twitter_data_file,'rows.item')
-
-# https://datahub.io/core/language-codes
-
-# get code:language mapping dict
-##### need to load the file through the argument.
-lang_map = util.get_language_code_map('./data/language-codes_csv.csv')
+# Loading the lnaguage codes
+lang_map = util.get_language_code_map(args.language_file)
 # loading the sydGrid.json file
-##### need to load the file through the argument.
-with open('./data/sydGrid.json', 'r', encoding='utf-8') as f:
+
+with open(args.grid_file_path, 'r', encoding='utf-8') as f:
     syd_grid = json.load(f)
 
 # This is so that all the process get to the same point in the code before getting the chunk sizes information.
@@ -87,20 +78,6 @@ while twitter_data_file.tell() < endindex:
         tweet_data.append(tweet_data_json)
         count+=1
 
-    #########################################
-    # TO BE FILLED IN / REPLACED WITH STREAM/CHUNK PROCESSING?
-    # this code block will change in multi-core setting
-    # maybe output format won't be a dictionary in multi-core setting
-    # can change accordingly once json stream/chunk parsing code is completed
-
-
-    ########################################
-
-    # extract key info from given dataset
-    # (may be changed little depending on how json chunk is given
-    # for the processor from code above)
-    # will probably write it as a function once input format is
-    # finalized
 
     rows_dict = {'tweet_id': [],
                  'language': [],
@@ -114,30 +91,8 @@ while twitter_data_file.tell() < endindex:
 
     # clean
     df_tweets = util.process_df(df_tweets, lang_map)
-    # insert artificial row with inivalid language code
-    # df_tweets.loc[df_tweets.shape[0]] = ['temp', 'zz', {
-    #     'type': 'Point', 'coordinates': [151.211, 33]}]
 
     unmathced = util.count_unmatched(df_tweets)
-
-    ############################################################
-    # TO BE CHANGED.
-    # EITHER RESOLVE BOUNDARY ISSUES BY KEEPING GEOPANDAS APPROACH,
-    # OR GO WITH FOR LOOP APPROACH (WHICH IS EASIER PROBABLY?)
-    # GRID LOCATION SHOULD BE APPENDED AS AN EXTRA COLUMN TO 'df_tweets'.
-    # KEEP THE DF NAME THE SAME FOR CONVENIENCE (tweets_with_cells) (doesn't really matter)
-    # also handy to keep 'cells_id' column name the same.
-
-    #######
-    # I have resolved the boundary issues by using the geopandas approach. There are some if-else statements that
-    # are involved in the end that I could not get rid of. Everything is same as it was before. The subset of
-    # the tweets which are not resolved by sjoin will go through a for loop and if-else statemnts to get resolved
-    # according to the rules mentioned in assignment. This way only the tweets which are on the boundary will go
-    # through the for loop and if-else statements.
-
-    # Also grid id are appended to the original df_tweets in a way.
-
-    # tweet-grid matching
 
 
     df_tweets = util.matching_grid(df_tweets,syd_grid)
@@ -168,7 +123,7 @@ if process_rank == 0:
     df_top10 = util.df_format_top_10(lang_counts)
     # final output df format
     df_final = df_total_tweets.merge(df_top10, on='cells_id')
-    df_final.to_csv('./results/output.csv', index=False)
+    df_final.to_csv(args.out_file, index=False)
 
     end_time = time.time() - start_time
     print(f'{end_time} secs')
